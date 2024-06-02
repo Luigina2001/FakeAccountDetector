e5f780e2791c42ec3d33b896c48ed60424d749f7
luigina <l.costante1@studenti.unisa.it>
2024-06-02 23:08:09 +0200

    aggiunta preprocessing instagram e general

---
+++ preprocessing/general/users.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', None)  # per mostrare tutte le colonne
+
+general = "../../datasets/general/users.csv"
+real_users = pd.read_csv(general, sep=',')
+# print(dataset.head())
+
+print("---------------------------------------------------------------------------------------------------------")
+
+nomi_campi = real_users.columns
+print(f"Nomi dei campi presenti nel dataset {nomi_campi}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+numero_campi = real_users.shape[1]
+print(f"Numero di campi presenti nel dataset: {numero_campi}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+lunghezza_dataset = real_users.shape[0]
+print(f"Lunghezza del dataset (numero di righe): {lunghezza_dataset}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+print("INFO")
+real_users.info()
+
+print("---------------------------------------------------------------------------------------------------------")
+
+statistiche = real_users.describe(include='all')
+print(f"STATISTICHE:\n{statistiche}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+colonna = "url"
+valori_unici = real_users[colonna].unique()  # calcolo i valori unici dalla colonna
+print(f"Valori unici colonna {colonna}: {len(valori_unici)}")
+
+print("---------------------------------------------------------------------------------------------------------")
---
+++ preprocessing/instagram/instagram2_2class.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', None)  # per mostrare tutte le colonne
+
+general = "../../datasets/instagram2/user_fake_authentic_2class.csv"
+ig_users = pd.read_csv(general, sep=',')
+# print(dataset.head())
+
+print("---------------------------------------------------------------------------------------------------------")
+
+nomi_campi = ig_users.columns
+print(f"Nomi dei campi presenti nel dataset {nomi_campi}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+numero_campi = ig_users.shape[1]
+print(f"Numero di campi presenti nel dataset: {numero_campi}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+lunghezza_dataset = ig_users.shape[0]
+print(f"Lunghezza del dataset (numero di righe): {lunghezza_dataset}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+print("INFO")
+ig_users.info()
+
+print("---------------------------------------------------------------------------------------------------------")
+
+statistiche = ig_users.describe(include='all')
+print(f"STATISTICHE:\n{statistiche}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+colonna = "class"
+valori_unici = ig_users[colonna].unique()  # calcolo i valori unici dalla colonna
+print(f"Valori unici colonna {colonna}: {valori_unici}")
+
+num_fake = ig_users[ig_users['class'] == 'f'].shape[0]
+num_real = ig_users[ig_users['class'] == 'r'].shape[0]
+print(f"Numero di account fake: {num_fake}")
+print(f"Numero di account real: {num_real}")
+
+print("---------------------------------------------------------------------------------------------------------")
